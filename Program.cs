988bf424ea1f8f9d144879658798f13ba9fc9280
KonstigSwe <swekonstig@gmail.com>
2020-11-05 10:59:07 +0100

    contrythingy

--- Program.cs
+++ Program.cs
@@ -9,27 +9,25 @@ namespace CarObjectTraning
 {
     class Program
     {
-        class Person
+       class Contry
         {
-            public string förnamn, efternamn, hemtelefon, jobbtelefon;
-            public Person(string S,string E,string H, string J)
-            {
-                förnamn = S;
-                efternamn = E;
-                hemtelefon = H;
-                jobbtelefon = J;
-            }
-            public void Print()
-            {
-                Console.WriteLine("{0} {1} {2} {3}", förnamn, efternamn, hemtelefon, jobbtelefon);
-                    
-            }
+          public  string name;
+          public  City capital;
+            string popuation;
+        }
+        class City
+        {
+           public string name;
+            string popuation;
         }
         static void Main(string[] args)
         {
-            Person Arne = new Person("arne", "saknusem", "1233315513", "75345644564253");
-            Arne.Print();
+            City Stockholm = new City();
+            Contry Sverige = new Contry();
+            Sverige.capital = Stockholm;
+
             Console.ReadKey();
+            
         }
     }
 }
